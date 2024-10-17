21a502e79500120a2d13c10d17cd8eb779e94d07
Ribeiro202 <138294377+Ribeiro202@users.noreply.github.com>
2024-10-17 17:39:37 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function openModal(imageSrc) {
+    const modal = document.getElementById("modal");
+    const modalImage = document.getElementById("modalImage");
+    modal.style.display = "flex";
+    modalImage.src = imageSrc;
+}
+
+function closeModal() {
+    const modal = document.getElementById("modal");
+    modal.style.display = "none";
+}
